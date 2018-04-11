6a0b046c3120bf8da2ec39dc4688556983364a28
Paul1987 <poginni@gmail.com>
2018-04-10 23:50:19 -0700

    Added try exec block to Archive service

--- video_analyzer/Archiver/Archiver.py
+++ video_analyzer/Archiver/Archiver.py
@@ -12,27 +12,20 @@ class Archiver():
 
     def archive(self):
         # Put all the files into a list
-        # try:
-        s3 = boto3.resource('s3')
-        posix_file_list = list(self.Path.glob('**/*.txt'))
-        # print(posix_file_list)
-        # posix_file_list = list(self.Path.glob('**/*.MOV'))            
+        try:
+            s3 = boto3.resource('s3')
+            posix_file_list = list(self.Path.glob('**/*.txt'))
+            # posix_file_list = list(self.Path.glob('**/*.MOV'))            
 
-        # Upload the files to S3
-        for i in range(len(posix_file_list)):    
-            s3.Object('image-recognition-analyzer-bucket', posix_file_list[i].parts[-1]).put(Body=open(posix_file_list[i], 'rb'))
+            # Upload the files to S3
+            for i in range(len(posix_file_list)):    
+                s3.Object('image-recognition-analyzer-bucket', posix_file_list[i].parts[-1]).put(Body=open(posix_file_list[i], 'rb'))
 
-            # Move files to processed folder
-            os.rename(str(posix_file_list[i].absolute()), str(self.processed_folder) + "/" + str(posix_file_list[i].parts[-1]))
-        # print(str(self.path_to_data) + str(posix_file_list[i].absolute()))
-        # print("posix_file_list: " + str(posix_file_list[i].absolute()))
-        # print("type: " + type(posix_file_list[i]))
-        # print("\n")
-        # print("self.path_to_data + posix_file_list[i]: " + self.path_to_data + posix_file_list[i])
-        # print("type: " + type(self.path_to_data + posix_file_list[i]))
+                # Move files to processed folder
+                os.rename(str(posix_file_list[i].absolute()), str(self.processed_folder) + "/" + str(posix_file_list[i].parts[-1]))
 
-        # except RuntimeError:
-        #     print("A runtime error has occured")
+        except RuntimeError:
+            print("A runtime error has occured")
 
 a = Archiver()
 a.archive()
--- video_analyzer/Archiver/archiver_config.py
+++ video_analyzer/Archiver/archiver_config.py
@@ -2,6 +2,5 @@
 path_to_data = "/Users/poginni/Streaming/test"
 processed_folder = "/Users/poginni/Streaming/processed"
 
-#
 # path_to_data = "/Users/poginni/Google\ Drive/Coding/AWS/Streaming/test"
 # processed_folder = "/Users/poginni/Google\ Drive/Coding/AWS/Streaming/processed"
