22b06598bcae439cd7cc1031e1f9837dd670dd7c
Ubuntu <ubuntu@ip-172-31-34-186.us-west-2.compute.internal>
2018-08-13 15:14:52 +0000

    Took away unused import & added better comments

--- video_analyzer/Archiver/Archiver.py
+++ video_analyzer/Archiver/Archiver.py
@@ -7,24 +7,23 @@ from pathlib import Path
 class Archiver():
 
     def __init__(self):
-        self.Path = Path(config.configurations['path_to_data'])
+        # Create a PosixPath object (named "path") that points to the directory of the pre-processed video
+        self.path = Path(config.configurations['path_to_data'])
+        # Identify the directory location where videos will be sent post-processing
         self.processed_folder = config.configurations["processed_folder"]
-
-        # self.Path = Path(config.path_to_data)
-        # self.processed_folder = config.processed_folder
 
     def archive(self):
         # Put all the files into a list
         try:
             s3 = boto3.resource('s3')
             # posix_file_list = list(self.Path.glob('**/*.txt'))
-            posix_file_list = list(self.Path.glob('**/*.MOV'))
+            posix_file_list = list(self.path.glob('**/*.MOV'))
 
             # Upload the files to S3
             for i in range(len(posix_file_list)):
                 s3.Object('image-recognition-analyzer-bucket', posix_file_list[i].parts[-1]).put(Body=open(posix_file_list[i], 'rb'))
 
-                # Move files to processed folder
+                # Move files to the post-processed folder
                 os.rename(str(posix_file_list[i].absolute()), str(self.processed_folder) + "/" + str(posix_file_list[i].parts[-1]))
 
         except RuntimeError:
